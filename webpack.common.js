9dcdf3d937e261d39dba5704a63e4d7ad6deab54
Amit Samanta <developer.amitsamanta@gmail.com>
2021-04-22 14:45:35 +0530

    Setup production build

---
+++ webpack.common.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        app: './src/app.js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }),
+    ],
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }, {
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: "css-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: "sass-loader",
+                    options: {
+                        sourceMap: true
+                    }
+                },
+            ],
+            test: /\.s?css$/
+        }]
+    }
+};
+
+// loader
---
+++ webpack.dev.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+    mode: 'development',
+    devtool: 'eval-cheap-module-source-map',
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 8008,
+        historyApiFallback: true
+    },
+});
